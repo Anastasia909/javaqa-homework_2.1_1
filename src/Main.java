3da5e339abee612a6d66f35dff6069f4683dfbcc
Anastasiya Babiy <nastasia909@gmail.com>
2020-04-08 00:34:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int Ticket = 38_925;
+        int MilesPrice = 20;
+        int Miles = Ticket / MilesPrice;
+        System.out.println(Miles);
+    }
+}

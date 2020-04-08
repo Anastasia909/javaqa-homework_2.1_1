8d07e8b9b782e3a5a9bcad420eb2bc7d4be97b83
Anastasiya Babiy <nastasia909@gmail.com>
2020-04-08 10:00:15 +0300

    Correction

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int Ticket = 38_925;
-        int MilesPrice = 20;
-        int Miles = Ticket / MilesPrice;
-        System.out.println(Miles);
+        int ticket = 38_925;
+        int milesPrice = 20;
+        int miles = ticket / milesPrice;
+        System.out.println(miles);
     }
 }
